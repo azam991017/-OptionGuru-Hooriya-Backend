52316a7decccb9c8e4784d71b20bd44ac3663fd6
azam991017 <itzicajmal@gmail.com>
2025-02-26 18:27:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "OptionGuru Hooriya Backend is running!"
+def index():
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 10000))  # Render dynamic port
-    app.run(host='0.0.0.0', port=port, debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
