f429320287b1ece93989b2ea375dd8d072ba2761
azam991017 <itzicajmal@gmail.com>
2025-02-26 18:21:22 +0530

    Create script.js

---
+++ static/script.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    alert('Welcome to OptionGuru Hooriya!');
+    
+    // Placeholder for real-time data logic
+    document.getElementById('buy-sell-signals').innerHTML = "<p>Buy/Sell Signals will be shown here</p>";
+    document.getElementById('watchlist').innerHTML = "<p>Watchlist will be shown here</p>";
+    document.getElementById('profit-loss-tracker').innerHTML = "<p>Profit/Loss Tracker will be shown here</p>";
+    document.getElementById('heatmap').innerHTML = "<p>Market Heatmap will be shown here</p>";
+});
